08faebbdb0100d8ba6d53ff9ab17fa4a8e672e21
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-12-16 13:48:18 +0100

    Custom NavLink component

---
+++ src/Components/NavLinks.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+const NavLinks = ({ navLink, color }) => {
+    // If a color prop is not passed, the default color of the NavLink is #fff
+
+        const returnColor = (val) => {
+        const colorTestReg = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+        if (
+          (color !== null || color !== undefined) &&
+          colorTestReg.test(val)
+        ) {
+          return val;
+        } else {
+          return '#fff';
+        }
+      };
+
+    const Link = styled.a`
+        color:${returnColor(color)};
+        padding-right:1rem;
+    `;
+
+    return (
+        <li><Link>{navLink}</Link></li>
+    );
+}
+
+export default NavLinks;
