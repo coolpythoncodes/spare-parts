aa0f46fbdc8a49a49d4ae85d8fafbe0c776c3431
coolpythoncodes <coolpythoncodes@gmail.com>
2020-12-24 16:52:46 +0100

    how it works is responsive on all screens

--- src/Components/Cards/WorkCard.js
+++ src/Components/Cards/WorkCard.js
@@ -4,21 +4,21 @@ import SettingsIcon from '@material-ui/icons/Settings';
 const WorkCard = ({ children }) => {
 
     const Card = styled.div`
-        width: 287px;
-        height: 278px;
-        background: #ECECEC;
-        border-radius: 10px;
         display: flex;
         flex-direction: column;
         align-items: center;
+        flex: 0.3;
+        min-height: 278px;
+        background: #ECECEC;
+        border-radius: 10px;
         margin-top: 2rem;
     `
 
     const Text = styled.p`
         font-family: Mulish;
-        font-size: 14px;
+        font-size: 1rem;
         line-height: 22px;
-        padding: 0 41px;
+        padding: 0 42px 48px 42px;
         overflow: hidden;
         
     `
